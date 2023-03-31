758de1af4ed658f164207f15099bb1fad7f22329
Bento Barbosa <103281382+bentob18@users.noreply.github.com>
2023-03-31 18:03:13 -0300

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,55 +1,85 @@
-import sys
-import nltk
-import pandas as pd
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.multioutput import MultiOutputClassifier
+# import libraries
 import numpy as np
-from nltk.tokenize import word_tokenize
-nltk.download(['punkt', 'wordnet'])
-from nltk.stem import WordNetLemmatizer
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+import sys
+import os
+import re
 from sqlalchemy import create_engine
-from sklearn.multioutput import MultiOutputClassifier
+import pickle
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sqlalchemy import create_engine
 import pickle
 
 def load_data(database_filepath):
-    '''
-    input:
-        database_filepath: File path where sql database was saved.
-    output:
-        X: Training message List.
-        Y: Training target.
-        category_names: Categorical name for labeling.
-    '''
-    engine = create_engine('sqlite:///'+ database_filepath)
-    df = pd.read_sql_table('FigureEight', engine)
-    X = df.message.values
-    Y = df[df.columns[4:]].values
-    categories = df.columns[4:].tolist()
-    return X, Y, categories
-
-def tokenize(message):
-    '''
-    input:
-        text: Message data for tokenization.
-    output:
-        clean_tokens: Result list after tokenization.
-    '''
+    """
+    Load Data from the Database Function
     
-    text = re.sub(r"[^a-zA-Z0-9]", "", text.lower())
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+    Arguments:
+        database_filepath -> Path to SQLite destination database (e.g. disaster_response_db.db)
+    Output:
+        X -> a dataframe containing features
+        Y -> a dataframe containing labels
+        category_names -> List of categories name
+    """
+    
+    engine = create_engine('sqlite:///' + database_filepath)
+    table_name = os.path.basename(database_filepath).replace(".db","") + "_table"
+    df = pd.read_sql_table(table_name,engine)
+    
+    #Remove child alone as it has all zeros only
+    df = df.drop(['child_alone'],axis=1)
+    
+    # Given value 2 in the related field are neglible so it could be error. Replacing 2 with 1 to consider it a valid response.
+    # Alternatively, we could have assumed it to be 0 also. In the absence of information I have gone with majority class.
+    df['related']=df['related'].map(lambda x: 1 if x == 2 else x)
+    
+    X = df['message']
+    y = df.iloc[:,4:]
+    #print(X)
+    #print(y.columns)
+    category_names = y.columns # This will be used for visualization purpose
+    return X, y, category_names
+
+def tokenize(text,url_place_holder_string="urlplaceholder"):
+    """
+    Tokenize the text function
+    
+    Arguments:
+        text -> Text message which needs to be tokenized
+    Output:
+        clean_tokens -> List of tokens extracted from the provided text
+    """
+    
+    # Replace all urls with a urlplaceholder string
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    
+    # Extract all the urls from the provided text 
+    detected_urls = re.findall(url_regex, text)
+    
+    # Replace url with a url placeholder string
+    for detected_url in detected_urls:
+        text = text.replace(detected_url, url_place_holder_string)
+
+    # Extract the word tokens from the provided text
+    tokens = nltk.word_tokenize(text)
+    
+    #Lemmanitizer to remove inflectional and derivationally related forms of a word
+    lemmatizer = nltk.WordNetLemmatizer()
+
+    # List of clean tokens
+    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
     return clean_tokens
 
 def build_model():
@@ -70,20 +100,25 @@ def build_model():
     'clf__estimator__min_samples_split': [2, 3, 4],
     'clf__estimator__criterion': ['gini', 'entropy']
 }
-    grid_search = GridSearchCV(pipeline, param_grid=parameters, cv=3, verbose=2)
-    return grid_search
-
-def evaluate_model(model, X_test, categories):
-    y_pred_df = pd.DataFrame(y_pred, columns=categories)
-    y_test_df = pd.DataFrame(y_test, columns=categories)
-    y_test_sel = y_test_df.loc[:, y_pred_df.columns]
-
-    for category in categories:
-        print("Category: {}".format(category))
-        print(classification_report(y_test_sel[category], y_pred_df[category]))
+    cv = GridSearchCV(pipeline, param_grid=parameters, cv=3, verbose=2)
+    return cv
+def evaluate_model(model, X_test, Y_test, category_names):
+    
+    Y_pred = model.predict(X_test)
+    print(classification_report(Y_test, Y_pred, target_names = category_names))
+    print('---------------------------------')
 
 def save_model(model, model_filepath):
-     joblib.dump(model, model_filepath)
+   
+    """
+    Save model as a pickle file
+    
+    Arguments:
+        model -> scikit-learn fitted model
+        model_filepath -> destination path to save .pkl file
+    """
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 def main():
     if len(sys.argv) == 3:
